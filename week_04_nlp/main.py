861bb37a3ac51433b55ec6f0058eb54029686204
Jan Kühn <yotka@posteo.org>
2023-11-06 21:30:32 +0100

    Improve script output

--- week_04_nlp/main.py
+++ week_04_nlp/main.py
@@ -2,6 +2,9 @@
 A Python Script to predict the artist of a song line
 """
 
+import os
+import sys
+
 import pandas as pd
 from includes import misc, modelling, parse, scrape
 from settings import conf
@@ -12,27 +15,36 @@ def main():
     Main function
     """
     if conf["scrape_song_list"]:
-        # Scrape HTML files containing URLs to song lyrics
+        print("Scraping HTML files containing URLs to song lyrics")
         scrape.scrape_artist_song_list(conf["artist_urls"])
+    else:
+        print("Skip scraping HTML files containing URLs to song lyrics")
 
     if conf["scrape_songs"]:
-        # Scrape lyric files from URLs
+        print("Scraping lyric files from URLs")
         scrape.scrape_songs_to_files(conf["artist_urls"])
+    else:
+        print("Skip scraping lyric files from URLs")
 
     if conf["parse_html"]:
-        # Parse lyrics from file and save them in a CSV file
+        print("Parsing lyrics from file and save them in a CSV file")
         songs = parse.parse_lyrics_from_files(conf["artist_urls"])
 
-        # Convert lyrics to lines
+        print("Converting lyrics to lines")
         misc.convert_lyrics_to_lines(songs)
+    else:
+        print("Skip parsing lyrics from file and save them in a CSV file")
 
-    # Import CSV file with lyrics
-    df_corpus = pd.read_csv(
-        conf["base_path"] + "data/" + "songs_by_line.csv", index_col=0
-    )
+    file_songs_by_line = conf["base_path"] + "data/" + "songs_by_line.csv"
+    if os.path.isfile(file_songs_by_line):
+        print("Importing CSV file with lyrics")
+        df_corpus = pd.read_csv(file_songs_by_line, index_col=0)
+    else:
+        print(f"Error: File not found. ({file_songs_by_line})")
+        sys.exit(1)
 
-    # Create wordclouds
     if conf["create_wordclouds"]:
+        print("Creating wordclouds")
         corpus = " ".join(df_corpus[df_corpus["artist"] == "Eels"]["lyrics"])
         misc.plot_wordcloud(corpus, name="Eels", shape="circle")
 
@@ -43,19 +55,26 @@ def main():
 
         corpus = " ".join(df_corpus[df_corpus["artist"] == "Adele"]["lyrics"])
         misc.plot_wordcloud(corpus, name="Adele", shape="circle")
+    else:
+        print("Skip creating wordclouds")
 
     if conf["train_model"]:
-        # Prepare corpus and labels
+        print("Train model")
+        print("Prepare corpus and labels")
         corpus, labels = modelling.prepare_corpus(df_corpus)
 
-        # Preprocess data
+        print("Preprocess data")
         corpus_clean = modelling.preprocess_corpus(corpus)
         assert len(corpus_clean) == len(corpus_clean)
 
         model = modelling.tune_hyperparameters(corpus_clean, labels)
 
-        # Fit the model with the vectorized data
+        print("Fit the model with the vectorized data")
         modelling.fit_model(model, corpus_clean, labels)
+    else:
+        print("Skip training model")
+
+    print("Done. Run predict.py to predict the artist of a song line.")
 
 
 if __name__ == "__main__":
